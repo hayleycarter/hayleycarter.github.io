542709fb9e2d598fe911af76bd89fd405ae35f23
Hayley Carter <17carterh@gmail.com>
2015-11-27 18:05:00 -0800

    nearly finished destinations page

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ $(document).ready(function() {;
 	})
 
 	$(".map-point").click(function() {
-		$(".map-point").hide();
+		$(".info-card").hide();
+		var number = $(this).attr('id').slice(-1);
+		$("#i" + number).show();
+		$("#dest-background").css({"background-color":"#39464A", "opacity":0.6})
+		$("#up-arrow").attr("src", "images/up-arrow-light.png");
+		$("html, body").animate({
+        	scrollTop: $("#i" + number).offset().top
+    		}, 2000);
+	})
+
+	$("#up-arrow").click(function() {
+  		$("html, body").animate({ scrollTop: 0 }, "slow")
+  		$("#dest-background").css({"background-color":"white", "opacity":0})
+  		$("#up-arrow").attr("src", "images/up-arrow.png");
 	})
 })
