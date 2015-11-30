1462dc633ccba502aa8282483d1b4f15b5b5fe5a
Hayley Carter <17carterh@gmail.com>
2015-11-29 22:32:48 -0800

    finished destination and semi completed homepage

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {;
+$(document).ready(function() {
 	$(".card-preview").hover(function() {
 		$(".image-info").fadeIn();
 	})
@@ -7,7 +7,7 @@ $(document).ready(function() {;
 		$(".info-card").hide();
 		var number = $(this).attr('id').slice(-1);
 		$("#i" + number).show();
-		$("#dest-background").css({"background-color":"#39464A", "opacity":0.6})
+		$("#dest-background").css({"background-color":"#39464A", "opacity":0.6});
 		$("#up-arrow").attr("src", "images/up-arrow-light.png");
 		$("html, body").animate({
         	scrollTop: $("#i" + number).offset().top
@@ -19,4 +19,10 @@ $(document).ready(function() {;
   		$("#dest-background").css({"background-color":"white", "opacity":0})
   		$("#up-arrow").attr("src", "images/up-arrow.png");
 	})
+
+	$("#homepage").mouseover(function() {
+		$("#logo-camera").animate({marginTop: "50px"}, 2000);
+	})
 })
+
+
